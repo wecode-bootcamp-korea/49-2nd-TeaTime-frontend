d1c7ba83840b7b573b5eea09855a4b57ce3d4d4a
박요진 <syon013@naver.com>
2023-09-23 22:34:28 +0900

    [etc] JsDoc 적용
    Button 에 JsDoc 적용시키기

--- src/Component/Button/Button.js
+++ src/Component/Button/Button.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Button.scss';
 
+/**
+ * Button Component
+ * @param className 버튼의 클래스명
+ * @param type 버튼의 타입 (button, submit)
+ * @param scale 버튼의 크기(small, medium, large)
+ * @param fullWidth width 100% 여부
+ * @param disabled 버튼의 비활성화 여부
+ * @param shape 버튼의 모양(fill, outline, tap)
+ * @param img 버튼의 이미지 (close, ...)
+ * @param color primary, secondary
+ */
 const Button = props => {
   const {
     className,
