1b815808ff4ccb316d6b499d3ffe3a109432c620
박요진 <syon013@naver.com>
2023-09-25 22:32:03 +0900

    [etc] payment Router 경로 추가

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import Products from './pages/Products/Products';
 import SignUp from './pages/SignUp/SignUp';
+import Payment from './pages/Payment/Payment';
 
 const Router = () => {
   return (
@@ -15,6 +16,7 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<Products />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
