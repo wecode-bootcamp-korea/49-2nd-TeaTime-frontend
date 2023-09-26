50d3f9cbe31051f4d72d6180335b0167d96324c3
박요진 <syon013@naver.com>
2023-09-26 10:15:15 +0900

    [etc] 베스트 상품 UI 생성을 위한 임시 커밋

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -4,8 +4,14 @@ import './Payment.scss';
 const Payment = () => {
   return (
     <div className="payment">
+      <div className="paymentHeader">
+        <h1>결제하기</h1>
+      </div>
       <div className="paymentContainer">
-        <p>payment</p>
+        <div className="paymentUserInfoTitle">
+          <h2 className="paymentUserInfo">주문고객정보</h2>
+          <p className="paymentUserName">홍길동/010-1215-8452</p>
+        </div>
       </div>
     </div>
   );
